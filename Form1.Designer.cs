9011db2dbffd82f5506c1de94ec18a1bf4eca135
Prasetyono Putra <prasetyonoptr@gmail.com>
2024-12-01 10:00:22 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Billing
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            groupBox = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label9 = new Label();
+            label13 = new Label();
+            label6 = new Label();
+            label10 = new Label();
+            label14 = new Label();
+            label7 = new Label();
+            label11 = new Label();
+            label15 = new Label();
+            label8 = new Label();
+            label12 = new Label();
+            label16 = new Label();
+            groupBox1 = new GroupBox();
+            label19 = new Label();
+            button1 = new Button();
+            label18 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label17 = new Label();
+            groupBox.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox
+            // 
+            groupBox.Controls.Add(label1);
+            groupBox.Controls.Add(label2);
+            groupBox.Controls.Add(label3);
+            groupBox.Controls.Add(label4);
+            groupBox.Controls.Add(label5);
+            groupBox.Controls.Add(label9);
+            groupBox.Controls.Add(label13);
+            groupBox.Controls.Add(label6);
+            groupBox.Controls.Add(label10);
+            groupBox.Controls.Add(label14);
+            groupBox.Controls.Add(label7);
+            groupBox.Controls.Add(label11);
+            groupBox.Controls.Add(label15);
+            groupBox.Controls.Add(label8);
+            groupBox.Controls.Add(label12);
+            groupBox.Controls.Add(label16);
+            groupBox.Location = new Point(12, 12);
+            groupBox.Name = "groupBox";
+            groupBox.Size = new Size(417, 277);
+            groupBox.TabIndex = 1;
+            groupBox.TabStop = false;
+            groupBox.Text = "Paket Internet";
+
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
+            label1.Location = new Point(94, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(214, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Warnet Ceria";
+
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label2.Location = new Point(36, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 28);
+            label2.TabIndex = 0;
+            label2.Text = "Nama Paket";
+
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label3.Location = new Point(201, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 28);
+            label3.TabIndex = 1;
+            label3.Text = "Waktu";
+
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label4.Location = new Point(301, 84);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 28);
+            label4.TabIndex = 2;
+            label4.Text = "Harga";
+
+            label5.AutoSize = true;
+            label5.Location = new Point(36, 129);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 3;
+            label5.Text = "Regular";
+
+            label9.AutoSize = true;
+            label9.Location = new Point(201, 129);
+            label9.Name = "label9";
+            label9.Size = new Size(37, 15);
+            label9.TabIndex = 4;
+            label9.Text = "1 Jam";
+
+            label13.AutoSize = true;
+            label13.Location = new Point(301, 129);
+            label13.Name = "label13";
+            label13.Size = new Size(54, 15);
+            label13.TabIndex = 5;
+            label13.Text = "Rp. 2.000";
+
+            label6.AutoSize = true;
+            label6.Location = new Point(36, 158);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Hemat";
+
+            label10.AutoSize = true;
+            label10.Location = new Point(201, 158);
+            label10.Name = "label10";
+            label10.Size = new Size(37, 15);
+            label10.TabIndex = 7;
+            label10.Text = "5 Jam";
+
+            label14.AutoSize = true;
+            label14.Location = new Point(301, 158);
+            label14.Name = "label14";
+            label14.Size = new Size(54, 15);
+            label14.TabIndex = 8;
+            label14.Text = "Rp. 9.000";
+
+            label7.AutoSize = true;
+            label7.Location = new Point(36, 189);
+            label7.Name = "label7";
+            label7.Size = new Size(47, 15);
+            label7.TabIndex = 9;
+            label7.Text = "Game 1";
+
+            label11.AutoSize = true;
+            label11.Location = new Point(201, 189);
+            label11.Name = "label11";
+            label11.Size = new Size(37, 15);
+            label11.TabIndex = 10;
+            label11.Text = "2 Jam";
+
+            label15.AutoSize = true;
+            label15.Location = new Point(301, 189);
+            label15.Name = "label15";
+            label15.Size = new Size(54, 15);
+            label15.TabIndex = 11;
+            label15.Text = "Rp. 5.000";
+
+            label8.AutoSize = true;
+            label8.Location = new Point(36, 220);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 15);
+            label8.TabIndex = 12;
+            label8.Text = "Game 2";
+
+            label12.AutoSize = true;
+            label12.Location = new Point(201, 220);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 15);
+            label12.TabIndex = 13;
+            label12.Text = "4 Jam";
+
+            label16.AutoSize = true;
+            label16.Location = new Point(301, 220);
+            label16.Name = "label16";
+            label16.Size = new Size(54, 15);
+            label16.TabIndex = 14;
+            label16.Text = "Rp. 9.000";
+
+            groupBox1.Controls.Add(label19);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label18);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label17);
+            groupBox1.Location = new Point(435, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(381, 277);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Detail Penggunaan";
+
+            label19.AutoSize = true;
+            label19.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            label19.Location = new Point(116, 220);
+            label19.Name = "label19";
+            label19.Size = new Size(158, 46);
+            label19.TabIndex = 5;
+            label19.Text = "00:00:00";
+
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button1.Location = new Point(219, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 50);
+            button1.TabIndex = 4;
+            button1.Text = "Mulai";
+            button1.UseVisualStyleBackColor = true;
+
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label18.Location = new Point(23, 121);
+            label18.Name = "label18";
+            label18.Size = new Size(113, 28);
+            label18.TabIndex = 3;
+            label18.Text = "Pilih Paket";
+
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(23, 152);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(159, 23);
+            comboBox1.TabIndex = 2;
+
+            textBox1.Location = new Point(23, 73);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 23);
+            textBox1.TabIndex = 1;
+
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label17.Location = new Point(23, 38);
+            label17.Name = "label17";
+            label17.Size = new Size(68, 28);
+            label17.TabIndex = 0;
+            label17.Text = "Nama";
+
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(830, 325);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox);
+            Name = "Form1";
+            Text = "Paket Internet Warnet Ceria";
+            groupBox.ResumeLayout(false);
+            groupBox.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private GroupBox groupBox;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label17;
+        private Label label18;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label19;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Billing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
